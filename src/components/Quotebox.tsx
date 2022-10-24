0e46df8c7fbbf478f72841c8f8906311e6d5f572
Liam <liam.stojanovic@gmail.com>
2022-10-24 13:46:44 -0400

    comments, twitter share, style cleanup

--- src/components/Quotebox.tsx
+++ src/components/Quotebox.tsx
@@ -6,12 +6,17 @@ interface Quote {
 }
 
 const Quotebox = () => {
-
+    /**
+     * State of Quotebox component is an instance of the Quote object
+     */
     const [quote, setQuote] = useState<Quote>({
         content: "Loading...",
         author: "Loading..."
     });
 
+    /**
+     * When the application first loads, fetch a quote
+     */
     useEffect(() => {
         const initialQuote = async () => await getRandomQuote();
 
@@ -19,7 +24,11 @@ const Quotebox = () => {
         .then((result) => setQuote(result));
 
     }, [])
-    
+
+    /**
+     * Leverages https://github.com/lukePeavey/quotable public API server to fetch a random quote
+     * @returns A Quote object
+     */
     const getRandomQuote = async (): Promise<Quote> => {
         try {
             const res = await fetch("https://api.quotable.io/random");
@@ -36,23 +45,46 @@ const Quotebox = () => {
             }
         }
     }
+    /**
+     * Uses state to generate an href suitable for Tweeting the quote and author
+     * @returns Twitter share href
+     */
+    const generateTwitterUrl = (): string => {
+        // https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/overview
+        const baseUrl: string = 'https://twitter.com/intent/tweet?text=';
+        // Construct the quote and author string using state
+        const quoteAndAuthor: string = `"${quote.content}" - ${quote.author}`;
+        // Concatenate baseUrl and quoteAndAuthor to yield tweet URL
+        const rawUrl: string = baseUrl.concat(quoteAndAuthor);
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/Document/createTextNode to escape HTML characters
+        const encodedQuote = document.createTextNode(rawUrl);
+        // Override null safety check, as there will always be non-null values in state.
+        return encodedQuote.textContent!;
+    }
 
     return (
         <div id="quote-box" className="flex flex-col items-center h-screen w-screen justify-center">
+            
             <div id="text" className="">
+                <i className="fa-sharp fa-solid fa-quote-left"></i>
                 {quote.content}
+                <i className="fa-sharp fa-solid fa-quote-right"></i>
             </div>
+            
 
             <div id="author" className="">
                 <b>{quote.author}</b>
             </div>
 
-            <button id="new-quote" className="" onClick={async () => setQuote(await getRandomQuote())}>
-                <i className="fa-solid fa-rotate-right"> New Quote</i>
+            <button id="new-quote" title="New Quote" onClick={async () => setQuote(await getRandomQuote())}>
+                <i className="fa-solid fa-rotate-right"></i> New Quote
             </button>
             
             <p id="social" className="">
-                <i className="fa-brands fa-square-twitter"></i>
+                <a id="tweet-quote" href={generateTwitterUrl()}>
+                    <i className="fa-brands fa-square-twitter"></i>
+                </a>
             </p>
         </div>
     )
